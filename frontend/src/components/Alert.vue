a50791b68784f88e894c5fe7e516dca8ce62f026
renato matos <renato.matos@specsavers.com>
2024-04-01 02:57:40 +0100

    fixing prop

--- frontend/src/components/Alert.vue
+++ frontend/src/components/Alert.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
-defineProps({
+import { computed } from 'vue'
+
+interface Props {
   message: String
+  type?: String
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  type: () => 'danger'
+})
+
+const alertClass = computed(() => {
+  return `alert alert-${props.type}`
 })
 </script>
 
 <template>
-  <div class="alert alert-danger" role="alert">{{ message }}</div>
+  <div :class="alertClass" role="alert">{{ message }}</div>
 </template>
